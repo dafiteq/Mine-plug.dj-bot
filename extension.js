ebeb95fbd7d1b1a1be88e826b0c80a9097e2a36c
dafiteq <david.arnolt@seznam.cz>
2016-11-18 09:44:28 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -112,6 +112,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/dafiteq/mine-plug.dj-bot/dafiteq-patch-1/extension.js", extend);
 
 }).call(this);
